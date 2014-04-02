45745879c10c86135e31f251525157743337d6e7
Nathan Buesgens <nathan@natb1.com>
2014-04-02 19:55:57 +0000

    more packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='mapping_tools',
+    description='tools for mapping domain models',
+    long_description=open('README.md').read(),
+    url='https://github.com/natb1/mapping_tools',
+    author='Nathan Buesgens',
+    author_email='nathan@natb1.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='development',
+    packages=['mapping_tools'],
+    install_requires=['sqlalchemy'], #TODO: limited feature set w/o sqla
+)
