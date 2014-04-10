cd2de2880ffa753f742f586f656122e65682a47c
Nathan Buesgens <nathan@natb1.com>
2014-04-10 15:38:36 -0400

    tweak to query interface

--- mapping_tools/query.py
+++ mapping_tools/query.py
@@ -2,8 +2,11 @@ class And:
     
     _criteria = []
 
-    def add(self, *path, **kwargs):
-        '''
-        kwargs: value
-        '''
-        self._criteria.append({'value':kwargs['value'], path:path})
+    def add(self, path, value, operator='in'):
+        if isinstanve(path, basestring):
+            path = (path,)
+        self._criteria.append(
+            {'value':value, 'path':path, 'operator'=operator})
+
+    def __iter__(self):
+        return iter(self._criteria)
