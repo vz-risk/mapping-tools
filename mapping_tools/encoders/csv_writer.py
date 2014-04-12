185b791b27cdcb094241ffc6f4c8160dd716f74e
Nathan Buesgens <nathan@natb1.com>
2014-04-12 01:58:07 -0400

    csv_writer implements column selection

--- mapping_tools/encoders/csv_writer.py
+++ mapping_tools/encoders/csv_writer.py
@@ -3,18 +3,25 @@ import sys
 
 class CSVWriter(object):
 
-    def __init__(self, mapping):
+    def __init__(self, mapping, columns=None):
         self.mapping = mapping
-        header = sorted([column.key for column in self.mapping.columns]) 
-        self.writer = csv.DictWriter(sys.stdout, header)
+        self.header = columns if columns is not None else \
+                      sorted([column.key for column in self.mapping.columns]) 
+        self.writer = csv.DictWriter(sys.stdout, self.header)
 
     def writeheader(self):
         self.writer.writeheader()
 
     def writerows(self, iterable):
         for obj in iterable:
-            row = self.mapping.composite_property.dump(obj)
+            row = self._filter_columns(
+                self.mapping.composite_property.dump(obj))
             self.writer.writerow(row)
+
+    def _filter_columns(self, row):
+        row = dict((key, value) 
+                   for key, value in row.items() if key in self.header)
+        return row
 
     def make_session(self):
         return Session(self.writeheader, self.writerows)
